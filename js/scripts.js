094467ce503b960cfbdcadee673ae5263998abab
rakeebhasan <mdrakeebhasan1@gmail.com>
2025-08-23 21:13:32 +0600

    My Cart Page Added With Responsiveness.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,39 @@
-(function($){
-	$(document).ready(function() {	
+// Product Add Functionality
+document.getElementById("addProduct").addEventListener("click", function () {
+	const totalProducts = parseInt(
+		document.getElementById("totalProducts").innerText
+	);
+	const newTotalProducts = totalProducts + 1;
+	document.getElementById("totalProducts").innerText = newTotalProducts;
+});
 
+// Product Remove Functionality
+document.getElementById("removeProduct").addEventListener("click", function () {
+	const totalProducts = parseInt(
+		document.getElementById("totalProducts").innerText
+	);
+	const newTotalProducts = totalProducts - 1;
+	if (totalProducts === 1) {
+		return;
+	}
+	document.getElementById("totalProducts").innerText = newTotalProducts;
+});
+
+(function ($) {
+	$(document).ready(function () {
 		// Scroll to Top
-		jQuery('.scrolltotop').click(function(){
-			jQuery('html').animate({'scrollTop' : '0px'}, 400);
+		jQuery(".scrolltotop").click(function () {
+			jQuery("html").animate({ scrollTop: "0px" }, 400);
 			return false;
 		});
-		
-		jQuery(window).scroll(function(){
+
+		jQuery(window).scroll(function () {
 			var upto = jQuery(window).scrollTop();
-			if(upto > 500) {
-				jQuery('.scrolltotop').fadeIn();
+			if (upto > 500) {
+				jQuery(".scrolltotop").fadeIn();
 			} else {
-				jQuery('.scrolltotop').fadeOut();
+				jQuery(".scrolltotop").fadeOut();
 			}
 		});
-
-
-		
-
-				
-		
-		
-		
-		
-		
-		
-		
-		
 	});
 })(jQuery);
